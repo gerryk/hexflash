47a896b69ca3d2357622ad6845b496b25d341499
Gerry Kavanagh <gerryk@gmail.com>
2015-04-14 22:44:15 +0100

    Added i2c read and write routines & switchable debug code

--- hexflash.ino
+++ hexflash.ino
@@ -1,3 +1,5 @@
+// #define DEBUG 1
+
 #include <Wire.h>                 //I2C library
 
 int led=13;
@@ -29,32 +31,69 @@ void loop (){
     char tmp[3];
     tmp[2] = '\0';
     quickFlash();
-    data.substring(1,3).toChar(buff, 2);
-    int byteCount=(int)strtol( buff, NULL, 16);
-    String offset=data.substring(3,6);
-    String recordType=data.substring(6,8);
-
+    data.substring(1,3).toCharArray(buff, 3);
+    int byteCount=(int)strtol(buff, NULL, 16);
+    String offset=data.substring(3,7);
+    data.substring(3,5).toCharArray(buff,3);
+    int msbOffset=(int)strtol(buff, NULL, 16);
+    data.substring(5,7).toCharArray(buff,3);
+    int lsbOffset=(int)strtol(buff, NULL, 16);
+    String recordType=data.substring(7,9);
+    data.substring(9,10+2*byteCount).toCharArray(buff,1+2*byteCount);  
+    #ifdef DEBUG
     Serial.print("Bytes: ");
     Serial.println(byteCount);
-    Serial.print("Offset: ");
+    Serial.print("HexOffset: ");
     Serial.println(offset);
+    Serial.print("DecOffset: ");
+    Serial.println(msbOffset*256+lsbOffset);
     Serial.print("Rectype: ");
     Serial.println(recordType);
     Serial.print("Datarec: ");
-    Serial.println(dataRec);
-    
-    for(int i=0;i<byteCount*2;i+=2)  {
-      quickFlash();
-      Serial.print(dataBytes[i]);
-      Serial.println(dataBytes[i+1]);
-      tmp[0] = dataBytes[i];
-      tmp[1] = dataBytes[i+1];
-      decByte=strtol(tmp,0,16);
-      Serial.println(decByte);  
+    Serial.println(buff);
+    #endif
+    if(recordType=="00")  {
+      for(int i=0;i<byteCount*2;i+=2)  {
+        #ifdef DEBUG
+        quickFlash();
+        Serial.print("Byte: ");
+        Serial.print(i/2);      
+        Serial.print(" : ");
+        Serial.print(buff[i]);
+        Serial.print(buff[i+1]);
+        #endif
+        tmp[0] = buff[i];
+        tmp[1] = buff[i+1];
+        decByte=strtol(tmp,0,16);
+        #ifdef DEBUG
+        Serial.print(" Offset: ");
+        Serial.print(msbOffset*256+lsbOffset+i/2);      
+        Serial.print(" :: ");
+        Serial.println(decByte);  
+        #endif
+        #ifndef DEBUG
+        writeEEPROM(0x50,msbOffset*256+lsbOffset+i,decByte);
+        #endif
+      }
+      for(int j=0;j<16;j++)  {
+        Serial.println("Reading Bytes");
+        Serial.println(i2c_eeprom_read_byte(0x50,msbOffset*256+lsbOffset+j));
+      }
     }
   }
 }
 
+
+byte i2c_eeprom_read_byte( int deviceaddress, unsigned int eeaddress ) {
+    byte rdata = 0xFF;
+    Wire.beginTransmission(deviceaddress);
+    Wire.write((int)(eeaddress >> 8)); // MSB
+    Wire.write((int)(eeaddress & 0xFF)); // LSB
+    Wire.endTransmission();
+    Wire.requestFrom(deviceaddress,1);
+    if (Wire.available()) rdata = Wire.read();
+    return rdata;
+  }
 
 void writeEEPROM(int deviceaddress, unsigned int eeaddress, byte data ) 
 {
@@ -63,6 +102,7 @@ void writeEEPROM(int deviceaddress, unsigned int eeaddress, byte data )
   Wire.write((int)(eeaddress & 0xFF)); // LSB
   Wire.write(data);
   Wire.endTransmission();
+  oneFlash();
   delay(5);
 }
 
